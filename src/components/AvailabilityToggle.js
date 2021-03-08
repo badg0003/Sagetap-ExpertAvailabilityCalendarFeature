75cc48adcf8e63e7b6f000889a6399876c88234d
Mike Badgley <mike@digiteam.ca>
2021-03-08 18:12:29 -0500

    UI: Toggle

--- src/components/AvailabilityToggle.js
+++ src/components/AvailabilityToggle.js
@@ -1,9 +1,11 @@
+import '../styles/toggle.scss'
+
 const AvailabilityToggle = ({ onHandleToggle, day, value }) => {
     return (
-        <>
-            <input type="checkbox" onChange={(e) => onHandleToggle(day, e.target.checked)} checked={value} />
-            {/* <label for="">Toggle</label> */}
-        </>
+        <div className="o-toggle">
+            <input className="visuallyhidden" id={`toggle-${day}`} type="checkbox" onChange={(e) => onHandleToggle(day, e.target.checked)} checked={value} />
+            <label htmlFor={`toggle-${day}`}><span class="visuallyhidden">Toggle {day} on/off</span></label>
+        </div>
     )
 }
 
