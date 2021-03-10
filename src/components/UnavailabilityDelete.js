28c4f3cc78b80c9e6ccc43793e8560b9244a9b9c
Mike Badgley <mike@digiteam.ca>
2021-03-09 22:43:14 -0500

    Add/delete icon updates

--- src/components/UnavailabilityDelete.js
+++ src/components/UnavailabilityDelete.js
@@ -3,9 +3,9 @@ const UnavailabilityDelete = ({ id, onHandleClick }) => {
         <>
             <button className="o-buttonicon" type="button" onClick={() => onHandleClick(id)}>
                 <span className="visuallyhidden">Delete this entry</span>
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7.99967 1.33325C4.31301 1.33325 1.33301 4.31325 1.33301 7.99992C1.33301 11.6866 4.31301 14.6666 7.99967 14.6666C11.6863 14.6666 14.6663 11.6866 14.6663 7.99992C14.6663 4.31325 11.6863 1.33325 7.99967 1.33325ZM11.333 10.3933L10.393 11.3333L7.99967 8.93992L5.60634 11.3333L4.66634 10.3933L7.05967 7.99992L4.66634 5.60659L5.60634 4.66659L7.99967 7.05992L10.393 4.66659L11.333 5.60659L8.93967 7.99992L11.333 10.3933Z" fill="#D00606"/>
-                </svg>    
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="#008CF5" />
+                </svg>
             </button>
         </>
     )
